89e746a9223f9de75fe8019d66e6927f4d87cee7
ZAHEER-PASHA <127183117+ZAHEER-PASHA@users.noreply.github.com>
2026-08-17 22:03:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,35 @@
-pipeline {
-    agent any
-
-    stages {
-
-        stage('Terraform Init') {
-            steps {
-                bat 'terraform init'
-            }
-        }
-
-        stage('Terraform Format Check') {
-            steps {
-                bat 'terraform fmt -check'
-            }
-        }
-
-        stage('Terraform Validate') {
-            steps {
-                bat 'terraform validate'
-            }
-        }
-
-        stage('Terraform Plan') {
-            steps {
-                bat 'terraform plan'
-            }
-        }
-
-        stage('Approval') {
-            steps {
-                input message: 'Do you want to apply Terraform changes?', ok: 'Apply'
-            }
-        }
-
-        stage('Terraform Apply') {
-            steps {
-                bat 'terraform apply -auto-approve'
-            }
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.59"
     }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+
+  s3_use_path_style = true
+}
+
+resource "aws_s3_bucket" "cicd" {
+  bucket = var.bucket_name
+
+  tags = {
+    Project     = var.project_name
+    ManagedBy   = "Terraform"
+    Environment = "Dev"
+  }
 }
